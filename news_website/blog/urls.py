351c7034c730ad75b840f5f4fdb623724def19b3
Teacher-2 <Teacher-2@DESKTOP-HKA31EE>
2024-07-31 20:17:42 +0330

    add categor

--- news_website/blog/urls.py
+++ news_website/blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'blog'
 urlpatterns = [
     path('',post_list, name = 'post_list'),
     path('category/<slug:category_slug>/',post_list, name = 'category_list'),
+    path('author/<int:author_pk>/',post_list, name = 'author_list'),
     path('post/<int:year>/<int:month>/<int:day>/<slug:slug>/',post_detail, name = 'post_detail')
 ]
--- news_website/blog/views.py
+++ news_website/blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from .models import Post, Category
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 # Create your views here.
 
-def post_list(request, category_slug = None):
+def post_list(request, category_slug = None, author_pk = None):
     posts = Post.objects.filter(status = 'published')
     if category_slug:
         category = get_object_or_404(Category,slug = category_slug)
         posts = posts.filter(category = category)
+
+    if author_pk:
+        user = get_object_or_404(User, pk = author_pk)
+        posts = posts.filter(author = user)
     return render(request,'blog/index.html',{'posts':posts})
 
 def post_detail(request,year,month,day,slug):
