f709b161aa4a8769b79c7170c4e74f2155260c70
Teacher-2 <Teacher-2@DESKTOP-HKA31EE>
2024-07-31 18:02:58 +0330

    change base.html and index.html

--- news_website/blog/admin.py
+++ news_website/blog/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Category
 
+admin.site.register(Category)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','body','author','publish','created','updated','status',)
+    list_display = ('title','author','publish','created','updated','status',)
     list_filter = ('author','publish','status',)
     search_fields = ('title','body')
     prepopulated_fields = {'slug':('title',)}
--- news_website/blog/models.py
+++ news_website/blog/models.py
@@ -2,14 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=50,verbose_name='عنوان دسته بندی')
+    slug = models.SlugField(max_length=300,unique=True)
+    is_visible = models.BooleanField(default= True)
+    position = models.IntegerField()
+
+    class Meta:
+        ordering = ('position',)
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+
+    def __str__(self):
+        return self.title
+
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft','پیشنویس'),
         ('published','منتشر شده')
     )
     title = models.CharField(max_length=250,verbose_name='عنوان')
+    category = models.ManyToManyField(Category, verbose_name='دسته بندی')
     thumbnail = models.ImageField(upload_to= 'image', null = True, blank=True, verbose_name='تصویر')
-    slug = models.SlugField(max_length=300)
+    slug = models.SlugField(max_length=300, unique_for_date= True)
     body = models.TextField(verbose_name='متن',null = True)
     author = models.ForeignKey(User,on_delete=models.CASCADE,
                                related_name='blog_posts',
